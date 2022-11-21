60b1ebb220620d21cbfb84d483d169d833ec9357
nathaliedees <nathaliedees@hotmail.com>
2022-11-21 09:56:36 +0100

    update data-preparation.py to run in linux
    update bayesian data prep script to run in linux

--- scripts/bayesian-modelling/data-preparation/data-preparation.py
+++ scripts/bayesian-modelling/data-preparation/data-preparation.py
@@ -16,14 +16,24 @@ import os
 import matplotlib.pyplot as plt
 
 #loading data
-cms = xr.open_dataset(
-    r"p:\11206887-012-sito-is-2021-so-et-es\Data\CMEMS_Satellite_100mx100m\merged_2015-2020\2015_2020_P1D_HROC_L3-transp_NWS_31_32ULE_100m-v01.nc")
-dfm = xr.open_dataset(
-    r"p:\11206887-012-sito-is-2021-so-et-es\Scripts\DFM_postprocess\data\input\DWSM-FM_100m_0000_map_regular_2367_1583_allvars_2timesteps.nc")
+from sys import platform
+if platform == 'linux':
+    cms = xr.open_dataset(
+        r"/p/11206887-012-sito-is-2021-so-et-es/Data/CMEMS_Satellite_100mx100m/merged_2015-2020/2015_2020_P1D_HROC_L3-transp_NWS_31_32ULE_100m-v01.nc")
+    dfm = xr.open_dataset(
+        r"/p/11206887-012-sito-is-2021-so-et-es/Data/DFM_DWSM-FM_100m/Postprocessing_maps/DWSM-FM_100m_0000_map_regular_500_400_allvars.nc")
+    opath = r"/p/11206887-012-sito-is-2021-so-et-es/Data/interpolated_CMS/CMS_interpolated_to_DFMgrid.nc"
+else:
+    cms = xr.open_dataset(
+        r"p:\11206887-012-sito-is-2021-so-et-es\Data\CMEMS_Satellite_100mx100m\merged_2015-2020\2015_2020_P1D_HROC_L3-transp_NWS_31_32ULE_100m-v01.nc")
+    dfm = xr.open_dataset(
+        r"P:\11206887-012-sito-is-2021-so-et-es\Data\DFM_DWSM-FM_100m\Postprocessing_maps\DWSM-FM_100m_0000_map_regular_500_400_allvars.nc")
+    opath = r"P:\11206887-012-sito-is-2021-so-et-es\Data\interpolated_CMS\CMS_interpolated_to_DFMgrid.nc"
+
 
 #combining mwtl locations and mwtl SPM to one tabel
-mwtl_loc=pd.read_csv(r'P:\11206887-012-sito-is-2021-so-et-es\Data\MWTL\MWTL_stations_all.csv')
-mwtl_dir=r'P:\11206887-012-sito-is-2021-so-et-es\Data\MWTL\csv_per_station'
+#mwtl_loc=pd.read_csv(r'P:\11206887-012-sito-is-2021-so-et-es\Data\MWTL\MWTL_stations_all.csv')
+#mwtl_dir=r'P:\11206887-012-sito-is-2021-so-et-es\Data\MWTL\csv_per_station'
 
 mwtl_loc=mwtl_loc.rename(columns={'geometriepunt.x':'lon', 'geometriepunt.y':'lat'})
 mwtl=[]
@@ -48,18 +58,23 @@ dfmspm = dfm.isel(time=0,layer=0).drop(['time','layer'])
 #dfmspm = dfm.drop(['layer']).mesh2d_water_quality_output_9
 
 # cutting to only 2017 # cutting out januari as well as it has a lot of nan data 
-cms_cut   = cms.sel(time=slice('2017-02-01','2017-12-31'))
+cms_cut   = cms.sel(time=slice('2017-01-01','2017-12-31'))
 
 #some tests on slice of the data. can be done on all when this works. 
-cms1=cms_cut.sel(time=slice('2017-3-01','2017-4-01'))
+#cms1=cms_cut.sel(time=slice('2017-3-01','2017-4-01'))
 
 #interpolating satellite data onto the model data, specifying the type of method
-cms_newgrid = cms1.interp(lat = dfmspm.lat, lon = dfmspm.lon, method='nearest')
+cms_newgrid = cms_cut.interp(lat = dfmspm.lat, lon = dfmspm.lon, method='nearest')
 
-#plotting
+#plotting an example of the interpolated gridconda 
 #cms_newgrid.isel(time=5).SPM.plot(vmin=0, vmax=100)
 
+#saving
 #when saving, add data encoding as explained in merge-cmems.py 
+cms_newgrid.SPM.encoding.pop('coordinates')
+cms_newgrid.to_netcdf(opath, encoding={'lat': cms_newgrid.lat.encoding, 'lon': cms_newgrid.lon.encoding, 'time': cms_newgrid.time.encoding, 'SPM': cms_newgrid.SPM.encoding})
+
 
 #checking which mwtl data is taken as the same time as the satellite data. #TODO
-merge['time_diff'] = abs((merge.Date_time - merge.datetimeUTC).astype('timedelta64[m]')).values
+
+#merge['time_diff'] = abs((merge.Date_time - merge.datetimeUTC).astype('timedelta64[m]')).values
