edb5f7ce503746b03dfe28f06e3099f6ccf8345f
nathaliedees <nathaliedees@hotmail.com>
2022-12-02 16:51:12 +0100

    Update bayesian_modelling.R
    updating R script

--- scripts/bayesian-modelling/modelling/bayesian_modelling.R
+++ scripts/bayesian-modelling/modelling/bayesian_modelling.R
@@ -1,7 +1,7 @@
 library("bmstdr")
 library("ncdf4")
 library("spTimer")
-
+library('tidyverse')
 # basic set up hybrid spm
 # input needed for Bsptime (package in bmstdr): 
 #   Data structure: 
@@ -18,13 +18,17 @@ library("spTimer")
 # input is space -> gridcel number for example
 # time
 # coordinates
-f2 <- y8hrmax ~ xmaxtemp+xwdsp+xrh
-f <- mwtl ~ time + spmmodel + spmsatellite
+d <- read_delim(file = "P://11206887-012-sito-is-2021-so-et-es/Data/input_bayesian/input_bayesian_mwtl_dfm_cms.csv")
+#f2 <- y8hrmax ~ xmaxtemp+xwdsp+xrh
+d[1] <- NULL
+data <-d
+head(data)
+f <- mwtl_SPM ~ sat_SPM + dfm_SPM
 
 #Bsptime running 3.2, 3.4 -> example
-M3 <- Bsptime(package="spTimer", formula=f2, data=nysptime, n.report=5, 
-              coordtype="utm", coords=4:5)
-
+M3 <- Bsptime(package="spTimer", formula=f, data=data, n.report=5, time.data=1,
+              coordtype="lonlat", coords=5:6)
+Bsptime()
 #selecting validation points
 
 #exploring the separable model according to 3.3
