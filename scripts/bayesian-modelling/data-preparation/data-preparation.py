a0ec1314995118856554bb0dd7707618cdfa6e12
nathaliedees <nathaliedees@hotmail.com>
2022-12-02 10:44:39 +0100

    Update data-preparation.py

--- scripts/bayesian-modelling/data-preparation/data-preparation.py
+++ scripts/bayesian-modelling/data-preparation/data-preparation.py
@@ -14,6 +14,7 @@ import xarray as xr
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import datetime
 
 #loading data
 from sys import platform
@@ -30,10 +31,9 @@ else:
         r"P:\11206887-012-sito-is-2021-so-et-es\Data\DFM_DWSM-FM_100m\Postprocessing_maps\DWSM-FM_100m_0000_map_regular_500_400_allvars.nc")
     opath = r"P:\11206887-012-sito-is-2021-so-et-es\Data\interpolated_CMS\CMS_interpolated_to_DFMgrid.nc"
 
-
 #combining mwtl locations and mwtl SPM to one tabel
-#mwtl_loc=pd.read_csv(r'P:\11206887-012-sito-is-2021-so-et-es\Data\MWTL\MWTL_stations_all.csv')
-#mwtl_dir=r'P:\11206887-012-sito-is-2021-so-et-es\Data\MWTL\csv_per_station'
+mwtl_loc=pd.read_csv(r'P:\11206887-012-sito-is-2021-so-et-es\Data\MWTL\MWTL_stations_all.csv')
+mwtl_dir=r'P:\11206887-012-sito-is-2021-so-et-es\Data\MWTL\csv_per_station'
 
 mwtl_loc=mwtl_loc.rename(columns={'geometriepunt.x':'lon', 'geometriepunt.y':'lat'})
 mwtl=[]
@@ -52,7 +52,7 @@ for fname in os.listdir(mwtl_dir):
     mwtl.append(spm)
 
 mwtl = pd.concat(mwtl)
-    
+
 #grid reprojection
 dfmspm = dfm.isel(time=0,layer=0).drop(['time','layer'])
 #dfmspm = dfm.drop(['layer']).mesh2d_water_quality_output_9
@@ -71,10 +71,21 @@ cms_newgrid = cms_cut.interp(lat = dfmspm.lat, lon = dfmspm.lon, method='nearest
 
 #saving
 #when saving, add data encoding as explained in merge-cmems.py 
-cms_newgrid.SPM.encoding.pop('coordinates')
-cms_newgrid.to_netcdf(opath, encoding={'lat': cms_newgrid.lat.encoding, 'lon': cms_newgrid.lon.encoding, 'time': cms_newgrid.time.encoding, 'SPM': cms_newgrid.SPM.encoding})
+#[cms.SPM.encoding.pop(b) for b in ['szip', 'zstd', 'bzip2', 'blosc']]
+[cms.lon.encoding.pop(b) for b in ['szip', 'zstd', 'bzip2', 'blosc']]
+[cms.lat.encoding.pop(b) for b in ['szip', 'zstd', 'bzip2', 'blosc']]
+[cms.time.encoding.pop(b) for b in ['szip', 'zstd', 'bzip2', 'blosc']]
+cms_newgrid.to_netcdf(
+    opath, 
+    # encoding={
+    #     'lat': cms.lat.encoding, 'lon': cms.lon.encoding, 
+    #     'time': cms.time.encoding, 'SPM': cms.SPM.encoding
+    #     }
+    )
 
 
 #checking which mwtl data is taken as the same time as the satellite data. #TODO
-
+cms_time=cms_cut.time.to_dataframe()
+mwtl[mwtl['Time']]
+new_date = mwtl['Time'].replace(minute=0, second=0, microsecond=0) + datetime.timedelta(hours=1)
 #merge['time_diff'] = abs((merge.Date_time - merge.datetimeUTC).astype('timedelta64[m]')).values
