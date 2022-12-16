cb1e71418b816caca0b6cbaeb8a74de0c9b8d3b2
rs9w33 <robyn.gwee@deltares.nl>
2022-12-16 17:28:01 +0100

    minor additions

--- scripts/bayesian-modelling/data-preparation/data-preparation.py
+++ scripts/bayesian-modelling/data-preparation/data-preparation.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Nov  7 16:23:05 2022
 
-@author: gwee
+@author: gwee, dees
 
 Bayesian modelling
 * To create a table ready for modelling, first reproject satellite data to dfm model grid
@@ -73,69 +73,74 @@ cms_newgrid = cms_cut.interp(lat = dfmspm.lat, lon = dfmspm.lon, method='nearest
 
 #saving data
 #when saving, add data encoding as explained in merge-cmems.py 
-#[cms.SPM.encoding.pop(b) for b in ['szip', 'zstd', 'bzip2', 'blosc']]
+# [cms.SPM.encoding.pop(b) for b in ['szip', 'zstd', 'bzip2', 'blosc']]
 # [cms.lon.encoding.pop(b) for b in ['szip', 'zstd', 'bzip2', 'blosc']]
 # [cms.lat.encoding.pop(b) for b in ['szip', 'zstd', 'bzip2', 'blosc']]
 # [cms.time.encoding.pop(b) for b in ['szip', 'zstd', 'bzip2', 'blosc']]
-#cms_newgrid.to_netcdf(
-#    opath, 
+cms_newgrid.to_netcdf(
+    opath, 
     # encoding={
     #     'lat': cms.lat.encoding, 'lon': cms.lon.encoding, 
     #     'time': cms.time.encoding, 'SPM': cms.SPM.encoding
     #     }
-#    )
+    )
+
+sel = cms_newgrid.isel(time=51).where(
+    ((cms_newgrid.lat <= cms.lat.max()) & (cms_newgrid.lat >= cms.lat.min()) \
+    & (cms_newgrid.lon <= cms.lon.max()) & (cms_newgrid.lon >= cms.lon.min())),drop=True)
 
 
-#checking which mwtl data is taken as the same time as the satellite data. #TODO
-mwtl = mwtl[mwtl['Time'].dt.year == 2017]
-#mwtl=mwtl[mwtl['Time'].dt.hour.between(9,12)]
-mwtl = mwtl.sort_values(by='Time', ascending=True)
-#mwtl_date=mwtl['Time'].dt.date
 
-def satspm(ee):
-    mth = pd.Series(ee.Time).dt.month.values[0]
-    time = ee.Time
-    lat = ee.lat
-    lon = ee.lon
-    varspm = cms_newgrid.sel(time = cms_newgrid.time.dt.month == mth).mean(dim='time')
+# #checking which mwtl data is taken as the same time as the satellite data. #TODO
+# mwtl = mwtl[mwtl['Time'].dt.year == 2017]
+# #mwtl=mwtl[mwtl['Time'].dt.hour.between(9,12)]
+# mwtl = mwtl.sort_values(by='Time', ascending=True)
+# #mwtl_date=mwtl['Time'].dt.date
 
-    spm = varspm.sel(lat=lat, lon=lon,method='nearest').SPM.values.flatten()[0]
-   
-    ee['sat_SPM'] = spm
-   # ee['sat_time'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').time.values
-   # ee['sat_lon'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').lon.values
-    #ee['sat_lat'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').lat.values
+# def satspm(ee):
+#     mth = pd.Series(ee.Time).dt.month.values[0]
+#     time = ee.Time
+#     lat = ee.lat
+#     lon = ee.lon
+#     varspm = cms_newgrid.sel(time = cms_newgrid.time.dt.month == mth).mean(dim='time')
 
-    return ee
+#     spm = varspm.sel(lat=lat, lon=lon,method='nearest').SPM.values.flatten()[0]
+    
+#     ee['sat_SPM'] = spm
+#     # ee['sat_time'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').time.values
+#     # ee['sat_lon'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').lon.values
+#     #ee['sat_lat'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').lat.values
 
-def dfmspm_func(ee):
-    time = ee.Time
-    lat = ee.lat
-    lon = ee.lon
+#     return ee
 
-    spm = dfmt.sel(time=time, lat=lat, lon=lon,method='nearest').mesh2d_water_quality_output_9.values.flatten()[0]
-   
-    ee['dfm_SPM'] = spm
-   # ee['sat_time'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').time.values
-   # ee['sat_lon'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').lon.values
-    #ee['sat_lat'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').lat.values
+# def dfmspm_func(ee):
+#     time = ee.Time
+#     lat = ee.lat
+#     lon = ee.lon
 
-    return ee
+#     spm = dfmt.sel(time=time, lat=lat, lon=lon,method='nearest').mesh2d_water_quality_output_9.values.flatten()[0]
+    
+#     ee['dfm_SPM'] = spm
+#     # ee['sat_time'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').time.values
+#     # ee['sat_lon'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').lon.values
+#     #ee['sat_lat'] = cms.sel(time=time, lat=lat, lon=lon,method='nearest').lat.values
 
-#create monthly aggregated
-for m in range(1,13):
-    cms_m = cms_newgrid.sel(time=cms_newgrid.time.dt.month == m).mean(dim='time')
-   
-dfmt = dfm.isel(layer=0).drop(['layer'])
+#     return ee
 
-dd = mwtl.apply(satspm,axis=1)
-tt = dd[~dd.sat_SPM.isna()]
-tt=tt[~tt.mwtl_SPM.isna()]
+# #create monthly aggregated
+# for m in range(1,13):
+#     cms_m = cms_newgrid.sel(time=cms_newgrid.time.dt.month == m).mean(dim='time')
+    
+# dfmt = dfm.isel(layer=0).drop(['layer'])
 
-df = tt.apply(dfmspm_func,axis=1)
-df.reset_index(drop=True,inplace=True)
+# dd = mwtl.apply(satspm,axis=1)
+# tt = dd[~dd.sat_SPM.isna()]
+# tt=tt[~tt.mwtl_SPM.isna()]
 
-#df.to_csv(r'P:\11206887-012-sito-is-2021-so-et-es\Data\input_bayesian\input_bayesian_mwtl_dfm_cms.csv')
+# df = tt.apply(dfmspm_func,axis=1)
+# df.reset_index(drop=True,inplace=True)
+
+# #df.to_csv(r'P:\11206887-012-sito-is-2021-so-et-es\Data\input_bayesian\input_bayesian_mwtl_dfm_cms.csv')
 
 
-#merge['time_diff'] = abs((merge.Date_time - merge.datetimeUTC).astype('timedelta64[m]')).values
+# #merge['time_diff'] = abs((merge.Date_time - merge.datetimeUTC).astype('timedelta64[m]')).values
