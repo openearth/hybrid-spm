935c2b17ae287b1edc74a18718904af1c1e42c35
nathaliedees <nathaliedees@hotmail.com>
2023-09-18 09:18:10 +0200

    Update bayesian_modelling.R
    trying out things

--- scripts/bayesian-modelling/modelling/bayesian_modelling.R
+++ scripts/bayesian-modelling/modelling/bayesian_modelling.R
@@ -4,6 +4,9 @@ library("spTimer")
 library('tidyverse')
 library('ggsn')
 library('ggmap')
+library('akima')
+library('ggplot2')
+library('tidyr')
 
 #map <- map_data("netherlands")
 # basic set up hybrid spm
@@ -13,44 +16,60 @@ library('ggmap')
 #     Data with n sites and T times within each site should be organised in the order (s1,t1),(s1,t2)
 #   using either the spTimer or sptDyn packages OR following the linear equation given in example 2.4
 #
+train <-read_delim(file = "P://11206887-012-sito-is-2021-so-et-es/Data/input_bayesian/input_bayesian.csv")
+dummy <-read_delim(file="P://11206887-012-sito-is-2021-so-et-es/Data/input_bayesian/input_bayesian_mwtl_dfm_cms_finemodelgrid_restructured.csv")
+f <- mwtl_SPM ~ sat_SPM + dfm_SPM
 
+# Sort the dataframe based on the 'Age' column
+sorted_data <- train %>%
+  arrange(s.index)
 
 #reading datasets 
 # formula definition -> now taken from example
 # input is space -> gridcel number for example, time, coordinates
-M1
 
-M2 <- Bspatial(model="spat", formula=f, data=train, 
-               coordtype="lonlat", coords=4:5, phi=0.4)
+subset<-head(train,20)
+M1 <- Bsptime(model="lm", formula=f, data=sorted_data, scale.transform = "SQRT")
 
-M3 <- Bsptime(package="spTimer", formula=f, data=train, coords=,c("UTMx","UTMy"), scale.transform="SQRT",
-              coordtype="utm",n.reports=5)
+M2 <- Bspatial(model="separable", formula=f, data=sorted_data, 
+             coordtype="lonlat", coords=4:5, phi=0.4)
 
-post <- M3$fit
-library(spTimer)
-gpred <- predict(post, newdata=gridnysptime, newcoords=~Longitude+Latitude)
+M3 <- Bsptime(package="separable", formula=f, data=sorted_data, coords=,c("UTMx","UTMy"), scale.transform="SQRT",
+              coordtype="utm",n.report=5, time.data=sorted_data[,6])
+
+
+
+sitemeans <- function(a, sn, tn=62) { 
+  u <- matrix(a, nrow=sn, ncol=tn, byrow=T)
+  b <- apply(u, 1, mean)
+  as.vector(b)
+}
+
+post <- M1$fit
+
+gpred <- predict(model, newdata=train, newcoords=~Longitude+Latitude)
 
 #selecting validation points
-gpred <- predict(post, newdata=gridnysptime, newcoords=~Longitude+Latitude)
+gpred <- predict(post, newdata=grid_sat_dfm, newcoords=~Longitude+Latitude)
 u <- gpred$pred.samples
 v <- apply(u, 2, sitemeans, sn=100)
 a <- get_parameter_estimates(t(v)) 
-b <- data.frame(gridnyspatial[, 1:5], a) 
+b <- data.frame(grid_sat_dfm[, 1:5], a) 
 meanmat <- post$op
 sig2eps <-  post$sig2ep
 sige <- sqrt(sig2eps)
 itmax <- ncol(meanmat)
-nT <- nrow(nysptime)
+nT <- nrow(train)
 sigemat <- matrix(rep(sige, each=nT), byrow=F, ncol=itmax)
 a <- matrix(rnorm(nT*itmax), nrow=nT, ncol=itmax)
 ypreds <- meanmat + a * sigemat
 ypreds <-  (ypreds)^2
 v <- apply(ypreds, 2, sitemeans, sn=28)
 a <- get_parameter_estimates(t(v)) 
-fits <- data.frame(nyspatial[, 1:5], a)
+fits <- data.frame(train[, 1:5], a)
 b <- rbind(b, fits)
 coord <- nyspatial[, c("Longitude","Latitude")]
-library(akima)
+
 xo <- seq(from=min(coord$Longitude)-0.5, to = max(coord$Longitude)+0.8, length=200)
 yo <- seq(from=min(coord$Latitude)-0.25, to = max(coord$Latitude)+0.8, length=200)
 surf <- interp(b$Longitude, b$Latitude, b$mean,  xo=xo, yo=yo)
@@ -58,9 +77,9 @@ v <- fnc.delete.map.XYZ(xyz=surf)
 
 interp1 <- data.frame(long = v$x, v$z )
 names(interp1)[1:length(v$y)+1] <- v$y
-library(tidyr)
+
 interp1 <- gather(interp1,key = lat,value =Predicted,-long,convert = TRUE)
-library(ggplot2)
+
 nymap <- map_data(database="state",regions="new york")
 
 zr <- range(interp1$Predicted, na.rm=T)
